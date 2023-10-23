e4573813a09dea70bce4a9aec792fc54162df400
willydekeyser <ma074172@skynet.be>
2023-10-23 16:50:21 +0200

    Setup Resource Server

---
+++ SpringResourceServerCustomPasswordGrantType/src/main/java/willydekeyser/SpringResourceServerCustomPasswordGrantTypeApplication.java
@@ -0,0 +1,13 @@
+package willydekeyser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringResourceServerCustomPasswordGrantTypeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringResourceServerCustomPasswordGrantTypeApplication.class, args);
+	}
+
+}
