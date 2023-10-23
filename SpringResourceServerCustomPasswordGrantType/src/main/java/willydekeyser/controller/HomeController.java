10118fdfffde2e3f9af6630e41e741d1b0401752
willydekeyser <ma074172@skynet.be>
2023-10-23 16:58:55 +0200

    Update Resource Server

--- SpringResourceServerCustomPasswordGrantType/src/main/java/willydekeyser/controller/HomeController.java
+++ SpringResourceServerCustomPasswordGrantType/src/main/java/willydekeyser/controller/HomeController.java
@@ -1,5 +1,6 @@
 package willydekeyser.controller;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
 
 	@GetMapping("/")
-	public String home() {
-		return "Spring Resource Server 01";
+	public String home(Authentication authentication) {
+		return "Spring Resource Server 01: " + authentication.getName();
 	}
 }
